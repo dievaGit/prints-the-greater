f4b69f707726bcd3939b12abfe7a27ac6c9f0460
Dieva Leon <58702512+dievaGit@users.noreply.github.com>
2020-04-19 13:55:41 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaring variables
+            int a = 0, b = 0;
+
+            //Requesting data from the user
+            Console.Write(" Enter the first number: ");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write(" Enter the second number: ");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            if (a > b)
+            {
+                //Showing result
+                Console.WriteLine(" The greatest number is: {0}", a);
+
+            }
+            else
+            {
+                if (a < b)
+                {
+                    //Showing result
+                    Console.WriteLine(" The greatest number is: {0}", b);
+
+                }
+                else
+                {
+                    if (a == b)
+                    {
+                        //Showing result
+                        Console.WriteLine(" The numbers are equals.");
+
+                    }
+                }
+            }
+
+        }
+    }
+}
